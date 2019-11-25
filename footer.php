10b17c039d66b8861a210f5b793844cfa1a863bb
ryleybridges <rye.brid@gmail.com>
2019-11-26 11:48:19 +1300

    Nav editing

--- footer.php
+++ footer.php
@@ -1,25 +1,37 @@
-<?php if(has_nav_menu('bottom_navigation')): ?>
-    <nav class="navbar navbar-expand-md navbar-light navBottomColour" role="navigation">
-        <div class="container">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+<footer class="navBottomColour" style="width: 100%;">
+    <div class="row">
+        <div class="col">
+            <?php if(has_nav_menu('bottom_navigation')): ?>
+                <nav class="navbar navbar-expand-md navbar-light navBottomColour" role="navigation">
+                    <div class="container">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-            <?php
-                wp_nav_menu(array(
-                    'theme_location'  => 'bottom_navigation',
-                    'depth'           => 1, // 1 = no dropdowns, 2 = with dropdowns.
-                    'container'       => 'div',
-                    'container_class' => 'collapse navbar-collapse',
-                    'container_id'    => 'bs-example-navbar-collapse-1',
-                    'menu_class'      => 'navbar-nav mr-auto flex-column',
-                    'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
-                    'walker'          => new WP_Bootstrap_Navwalker(),
-                ));
-             ?>
+                        <?php
+                            wp_nav_menu(array(
+                                'theme_location'  => 'bottom_navigation',
+                                'depth'           => 1, // 1 = no dropdowns, 2 = with dropdowns.
+                                'container'       => 'div',
+                                'container_class' => 'collapse navbar-collapse',
+                                'container_id'    => 'bs-example-navbar-collapse-1',
+                                'menu_class'      => 'navbar-nav mr-auto flex-column',
+                                'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+                                'walker'          => new WP_Bootstrap_Navwalker(),
+                            ));
+                         ?>
+                    </div>
+                </nav>
+            <?php endif; ?>
         </div>
-    </nav>
-<?php endif; ?>
+        <div class="col text-right pr-5 pt-4">
+            <p>SHINE OFFICE:<br>(09) 815 4601</p>
+            <p>OFFICE HOURS:<br>MON - FRI 8:30 - 4:30PM</p>
+        </div>
+    </div>
+</footer>
+
+
 
 <?php wp_footer(); ?>
 </body>
--- page.php
+++ page.php
@@ -1,7 +1,14 @@
 <?php get_header(); ?>
 
-    <div class="container mt-0" id="mainBody">
-        
+    <div class="container" id="mainBody">
+        <div class="mb-3 p-2">
+            <?php if (have_posts()): ?>
+                <?php while(have_posts()): the_post(); ?>
+                    <h2 class="text-center"><?php the_title(); ?></h2>
+                    <?php the_content(); ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
     </div>
 
 <?php get_footer(); ?>
