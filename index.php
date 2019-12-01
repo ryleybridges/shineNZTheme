00b905ab2e85c3516382b86e5d1f7244ccef2dd1
ryleybridges <rye.brid@gmail.com>
2019-12-02 11:35:23 +1300

    Fixed woocommerce, include singular for index and included files to be edited for woocommerce

--- index.php
+++ index.php
@@ -7,15 +7,16 @@
 
                 <?php if(have_posts()): ?>
                     <?php while(have_posts()): the_post(); ?>
+
                         <div class="card mt-2 col-12 d-flex justify-content-center">
                             <div class="card-body">
                                 <div class="row">
                                     <?php if (!is_singular()): ?>
                                         <?php if(has_post_thumbnail()): ?>
-                                            <div class="col-md-3 col-12">
+                                            <div class="col-12 col-md-3">
                                                 <?php the_post_thumbnail(); ?>
                                             </div>
-                                            <div class="col-md-9 col-12">
+                                            <div class="col-12 col-md-9">
                                                 <h3><?php the_title(); ?></h3>
                                                 <p class="card-text"><?php the_excerpt(); ?></p>
                                                 <a href="<?php the_permalink(); ?>" class="btn btn-orange">Read More</a>
@@ -27,6 +28,12 @@
                                                 <a href="<?php the_permalink(); ?>" class="btn btn-orange">Read More</a>
                                             </div>
                                         <?php endif; ?>
+
+                                    <?php else: ?>
+                                        <div class="col-12">
+                                            <h3><?php the_title(); ?></h3>
+                                            <?php the_content(); ?>
+                                        </div>
                                     <?php endif; ?>
                                 </div>
                             </div>
